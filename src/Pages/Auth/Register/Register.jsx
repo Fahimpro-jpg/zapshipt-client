b15f4d0e1e0103bb81bf0317f95c613e4d6c8bf2
Fahimpro-jpg <fahadalamfahim111844@gmail.com>
2025-11-22 14:38:09 +0600

    set up the login and register page with firebase

--- src/Pages/Auth/Register/Register.jsx
+++ src/Pages/Auth/Register/Register.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import useAuth from '../../../Hooks/useAuth';
+import { Link } from 'react-router';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
 
     const {register,handleSubmit,formState:{errors}} = useForm()
 
+    const{registerUser}= useAuth()
+
     const handleRegistration = (data)=>{
         console.log('after register',data)
+        registerUser(data.email,data.password)
+        .then(result=>{
+            console.log(result.user)
+        })
+        .catch(error=>{
+            console.log(error)
+        })
     }
     return (
-        <div>
+       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl  mx-auto">
+        <h3 className="text-3xl text-center">Create an Account</h3>
+        <p className='text-center'>Please Register</p>
+      <div className="card-body">
+        
             <form onSubmit={handleSubmit(handleRegistration)}>
         <fieldset className="fieldset">
             {/* email */}
@@ -40,10 +56,16 @@ const Register = () => {
                 errors.password?.type==='pattern' && <p className='text-red-500'>Password Must have at least one uppercase,at least one lowercase,  at least one number, and at least one special characters</p>
             }
           <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Login</button>
+          <button className="btn btn-neutral bg-black text-white mt-4">Register</button>
+         
         </fieldset>
+        <p>Already have an account<Link
+        className='text-blue-400 underline'
+        to={'/login'}>Login</Link></p>
             </form>
-        </div>
+            <SocialLogin></SocialLogin>
+         </div>
+    </div>
     );
 };
 
